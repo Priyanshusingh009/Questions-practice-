b5a85c7425cb4e7f52e9e759cd72ee6b8a4c4840
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-05-30 00:30:31 +0530

    this is new pattern

---
+++ pattern2.java
@@ -0,0 +1,13 @@
+public class pattern2 {
+    public static void main(String[] args){
+        int rows=5;
+        for(int i =1;i<=rows;i++){
+            for(int col= 1;col<=i;col++){
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
