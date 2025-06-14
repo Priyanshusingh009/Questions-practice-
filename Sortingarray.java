27a1985ee17cccb5eb97f1ddbb47532a3a53c8a9
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-06-14 11:19:47 +0530

    sorting arrays

---
+++ Sortingarray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Sortingarray {
+    public static void main(String[] arg){
+        int[] Sort={43,67,88,23,88,90};
+        Arrays.sort(Sort);        // using methods Arrays.sort(arr);
+        for(int i :Sort){
+            System.out.print(i+" ");
+        }
+    }
+    
+}
