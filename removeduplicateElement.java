299f8b752703b43b1b717ecf930cf541334fa478
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-07-16 13:10:36 +0530

    removing  duplicate element of array

--- removeduplicateElement.java
+++ removeduplicateElement.java
@@ -1,24 +1,30 @@
 import java.util.Arrays;
 public class removeduplicateElement{
     public static void main(String[] args) {
-        int[] my_array={89,34,4,67,89,56,34,4,};
-        System.out.println("original array" + Arrays.toString(my_array));
-        Arrays.sort(my_array);
-        int x=0;
-        int[] new_array= new int[my_array.length];
-        for(int i=1;i<my_array.length;i++){
-            if(my_array[x]!=my_array[i]){
-                new_array[x]=my_array[x];
+        int[] arr = {4, 34, 89, 34, 4, 56, 89, 67};
+        System.out.println("origial array is :\n"+Arrays.toString(arr));
 
-                
+        Arrays.sort(arr); // Sort the array
+
+        int[] temp = new int[arr.length];
+        int x = 0;
+        temp[x] = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != temp[x]) {
                 x++;
-            }else{
-                x++;
-                continue;
+                temp[x] = arr[i];
             }
-
-
         }
-        System.out.println("modified array is " +Arrays.toString(new_array) );
+        //System.out.println(Arrays.toString(temp));
+
+        // Now create actual array with exact size
+        int[] uniqueArray = new int[x + 1];
+        for (int i = 0; i <= x; i++) {
+            uniqueArray[i] = temp[i];
+        }
+
+        // Now print the actual array
+        System.out.println("Array after removing duplicates: " + Arrays.toString(uniqueArray));
     }
 }
