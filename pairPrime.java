9d7befc6395a757dd21fb045fccb640ff5dbac14
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-07-21 23:40:51 +0530

    finding pairs of no.s whose sum is prime number

--- pairPrime.java
+++ pairPrime.java
@@ -8,7 +8,7 @@ public class pairPrime {
            
             for (int j = i + 1; j < inputArray.length; j++) {
                 // Check if the sum of inputArray[i] and inputArray[j] equals the prime.
-                if ((inputArray[i] + inputArray[j])%2 == 1 || (inputArray[i] + inputArray[j]) ==2  ) {
+        if ((inputArray[i] + inputArray[j])%2 == 1 && (inputArray[i] + inputArray[j])%3 != 1 && (inputArray[i] + inputArray[j])%3 != 0 && (inputArray[i] + inputArray[j])%5 != 0   && (inputArray[i] + inputArray[j])%7 != 0 ||(inputArray[i] + inputArray[j]) ==2) {
                     
                     System.out.println(inputArray[i] + " + " + inputArray[j] + " = " +(inputArray[i] + inputArray[j]));
                 }
@@ -17,8 +17,8 @@ public class pairPrime {
     }
     public static void main(String[] args) {
         pairPrime sum=new pairPrime();
-        sum.pairs_value(new int[]{23,43,65,56,87,90,10,20,15,5});
-        sum.pairs_value(new int[]{2, 7, 4, -5, 11, 5, 20});
+        sum.pairs_value(new int[]{23,9,72,43,65,56,87,90,10,20,15,5});
+       // sum.pairs_value(new int[]{2, 7, 4, -5, 11, 5, 20});
         
     }
     
