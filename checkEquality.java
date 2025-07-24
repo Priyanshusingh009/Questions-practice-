463358a9ad64f852bf3d261d54a66d5607ceb5af
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-07-24 10:39:26 +0530

    check whether the two arrays are ewual or not

--- checkEquality.java
+++ checkEquality.java
@@ -1,11 +1,13 @@
 public class checkEquality {
     public static void main(String[] args) {
         int[] array1 = {2, 5, 7, 8, 11};
-        int[] array2 = {2, 5, 7, 8, 11};
-        int len=array1.length;
-        for(int i =0;i<len;i++){
+        int[] array2 = {2, 5, 7, 8, 11,};
+        int len1=array1.length;
+        int len2=array2.length;
+        if(len1==len2){
+            for(int i =0;i<len1;i++){
             if(array1[i]==array2[i]){
-                if(i==len-1){
+                if(i==len1-1){
                     System.out.println("arrays are equal");
                 }
                 continue;
@@ -16,6 +18,13 @@ public class checkEquality {
                 break;
             }
         }
+
+        }
+        else{
+            System.out.println("arrays are not equal");
+        }
+
+        
     }
     
 }
