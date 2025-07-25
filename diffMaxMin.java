34fbf0c5e6b9d842bfbfa3da6080f93f9f753e72
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-07-25 21:09:05 +0530

    finding difference between minimum and maxium value of array

---
+++ diffMaxMin.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class diffMaxMin {
+    public static void main(String[] args) {
+        int[] arr={3,6,7,4,679,45,4};
+        Arrays.sort(arr);
+        int len = arr.length;
+        int result=arr[len-1]-arr[0];                                                   
+System.out.println("The differnce betwee max and min is  : " + result);
+        
+    }
+    
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
