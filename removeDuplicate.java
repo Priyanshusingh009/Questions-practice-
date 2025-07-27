b6521e1a27807d45607756c8fe605aab42cb708b
Priyanshusingh009 <er.priyanshusingh0332@gmail.com>
2025-07-27 13:31:41 +0530

    removing duplicate elemnts from array and find length of new array

---
+++ removeDuplicate.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class removeDuplicate{
+    public static void main(String[] args) {
+        
+        int[] array = {1, 2, 2, 3, 3, 3, 4, 5}; // sorted array
+
+        int[] new_arr = new int[array.length];
+        new_arr[0] = array[0];
+        int index = 1;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[i - 1]) {
+                new_arr[index] = array[i];
+                index++;
+            }
+        }
+
+        // Create final array of correct size
+        int[] result = new int[index];
+        for (int i = 0; i < index; i++) {
+            result[i] = new_arr[i];
+        }
+
+        System.out.println("The length of modified array is: " + result.length);
+        System.out.println("Modified array: " + Arrays.toString(result));
+    }
+       
+}
+    
+
